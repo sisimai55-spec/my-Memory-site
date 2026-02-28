60210193ed7866827d9944abf8cec524ad017170
sisimai55-spec <sisimai55@yahoo.co.jp>
2026-02-28 15:52:17 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
 const OWNER="sisimai55-spec";
 const REPO="my-photo-text-site";
-const TOKEN="GitHub Personal Access Token";
+
+/* GitHubトークン（あとで保存用に使う） */
+const TOKEN="ここはまだ空でOK";
 
 const DATA_FILE="data/data.json";
 const TOKEN_FILE="data/tokens.json";
