cfcbf09d0f5eb394cc2ab28a6ce5d2331bc8b76a
sisimai55-spec <sisimai55@yahoo.co.jp>
2026-02-28 15:34:29 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const OWNER="sisimai55-spec";
+const REPO="my-photo-text-site";
+const TOKEN="GitHub Personal Access Token";
+
+const DATA_FILE="data/data.json";
+const TOKEN_FILE="data/tokens.json";
