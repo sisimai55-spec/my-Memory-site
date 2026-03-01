da79cb23c40375b10054028637b515f10641abe9
sisimai55-spec <sisimai55@yahoo.co.jp>
2026-03-01 17:56:30 +0900

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,15 +1,59 @@
 async function post() {
 
-  const text = document.getElementById("text").value;
+  const text = textEl.value;
+  const file = photo.files[0];
   const user = localStorage.getItem("username");
+  const token = localStorage.getItem("token");
+
+  let image = null;
+
+  if (file) {
+    image = await compressImage(file);
+  }
 
   await fetch("/api/post", {
     method: "POST",
     body: JSON.stringify({
       user,
-      text
+      text,
+      image,
+      token
     })
   });
 
-  alert("投稿したよ！");
+  alert("投稿完了！");
+}
+
+// ⭐画像圧縮
+function compressImage(file) {
+  return new Promise(resolve => {
+
+    const img = new Image();
+    const reader = new FileReader();
+
+    reader.onload = e => img.src = e.target.result;
+
+    img.onload = () => {
+
+      const canvas = document.createElement("canvas");
+      const max = 800;
+
+      let w = img.width;
+      let h = img.height;
+
+      if (w > max) {
+        h *= max / w;
+        w = max;
+      }
+
+      canvas.width = w;
+      canvas.height = h;
+
+      canvas.getContext("2d").drawImage(img,0,0,w,h);
+
+      resolve(canvas.toDataURL("image/jpeg",0.7));
+    };
+
+    reader.readAsDataURL(file);
+  });
 }
