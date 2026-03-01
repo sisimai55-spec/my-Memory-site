e0d58f9e97b64e199850648a26a38b9a62cd7ef3
sisimai55-spec <sisimai55@yahoo.co.jp>
2026-03-01 17:45:49 +0900

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,25 +1,10 @@
-async function requireLogin(){
-
-const token=localStorage.getItem("loginToken");
-
-if(!token){
-location.href="login.html";
-throw "no login";
+function requireLogin(){
+  if(!localStorage.getItem("login")){
+    location.href="index.html";
+  }
 }
 
-const url=
-`https://api.github.com/repos/${OWNER}/${REPO}/contents/${TOKEN_FILE}`;
-
-const res=await fetch(url);
-const json=await res.json();
-
-const tokens=JSON.parse(
-decodeURIComponent(escape(atob(json.content)))
-);
-
-if(tokens[token]!=="active"){
-localStorage.removeItem("loginToken");
-location.href="login.html";
-throw "invalid";
-}
+function logout(){
+  localStorage.removeItem("login");
+  location.href="index.html";
 }
