bd049bcc1fe27d573287b6cdeceb48ba88594ece
sisimai55-spec <sisimai55@yahoo.co.jp>
2026-03-01 18:04:45 +0900

    Update view.js

--- js/view.js
+++ js/view.js
@@ -1,19 +1,57 @@
-async function loadPosts() {
+async function load(){
 
-  const res = await fetch(CONFIG.postsUrl);
-  const posts = await res.json();
+const res = await fetch(CONFIG.postsUrl);
+const posts = await res.json();
 
-  const area = document.getElementById("posts");
-  area.innerHTML = "";
+posts.reverse().forEach(p=>{
 
-  posts.reverse().forEach(p => {
-    area.innerHTML += `
-      <div class="post">
-        <b>${p.user}</b><br>
-        ${p.text}
-      </div>
-    `;
-  });
+postsEl.innerHTML += `
+<div class="post">
+
+<b>${p.user}</b><br>
+${p.text}<br>
+
+${p.image ?
+`<img src="${p.image}" onclick="openImg('${p.image}')">`
+:""}
+
+<br>
+
+❤️ ${p.likes}
+<button onclick="like('${p.id}')">いいね</button>
+
+${
+p.token===localStorage.getItem("token")
+? `<button onclick="del('${p.id}')">削除</button>`
+:""
 }
 
-loadPosts();
+</div>`;
+});
+}
+
+function like(id){
+fetch("/api/like",{method:"POST",body:JSON.stringify({id})})
+.then(load);
+}
+
+function del(id){
+fetch("/api/delete",{
+method:"POST",
+body:JSON.stringify({
+id,
+token:localStorage.getItem("token")
+})
+}).then(load);
+}
+
+function openImg(src){
+viewer.style.display="flex";
+bigImg.src=src;
+}
+
+function closeViewer(){
+viewer.style.display="none";
+}
+
+load();
