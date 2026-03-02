f2439fcaad39ba64f574e854e6e9b16d4af717da
sisimai55-spec <sisimai55@yahoo.co.jp>
2026-03-02 17:09:10 +0900

    Update view.js

--- js/view.js
+++ js/view.js
@@ -1,57 +1,17 @@
 async function load(){
 
-const res = await fetch(CONFIG.postsUrl);
-const posts = await res.json();
+ const res=await fetch(API_URL+"/api/posts");
+ const posts=await res.json();
 
-posts.reverse().forEach(p=>{
+ const area=document.getElementById("posts");
 
-postsEl.innerHTML += `
-<div class="post">
-
-<b>${p.user}</b><br>
-${p.text}<br>
-
-${p.image ?
-`<img src="${p.image}" onclick="openImg('${p.image}')">`
-:""}
-
-<br>
-
-❤️ ${p.likes}
-<button onclick="like('${p.id}')">いいね</button>
-
-${
-p.token===localStorage.getItem("token")
-? `<button onclick="del('${p.id}')">削除</button>`
-:""
-}
-
-</div>`;
-});
-}
-
-function like(id){
-fetch("/api/like",{method:"POST",body:JSON.stringify({id})})
-.then(load);
-}
-
-function del(id){
-fetch("/api/delete",{
-method:"POST",
-body:JSON.stringify({
-id,
-token:localStorage.getItem("token")
-})
-}).then(load);
-}
-
-function openImg(src){
-viewer.style.display="flex";
-bigImg.src=src;
-}
-
-function closeViewer(){
-viewer.style.display="none";
+ posts.reverse().forEach(p=>{
+   area.innerHTML+=`
+   <div class="container">
+   <b>${p.user}</b><br>
+   ${p.text}
+   </div>`;
+ });
 }
 
 load();
