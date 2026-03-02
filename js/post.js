8dc7d4cfe761fca78ca42fe76d2179bb4ad6145e
sisimai55-spec <sisimai55@yahoo.co.jp>
2026-03-02 17:29:58 +0900

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,18 +1,44 @@
-async function post(){
+async function post() {
 
- const text=document.getElementById("text").value;
+  const file = document.getElementById("image").files[0];
+  const text = document.getElementById("text").value;
+  const user = localStorage.getItem("loginUser");
 
- await fetch(API_URL+"/api/post",{
-  method:"POST",
-  headers:{
-   "Content-Type":"application/json"
-  },
-  body:JSON.stringify({
-   user:localStorage.username,
-   token:localStorage.token,
-   text:text
-  })
- });
+  let imageBase64 = "";
 
- alert("保存した！");
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = async function() {
+
+      imageBase64 = reader.result;
+
+      await sendPost(imageBase64);
+    };
+
+    reader.readAsDataURL(file);
+  } else {
+    await sendPost("");
+  }
+
+  async function sendPost(img) {
+
+    await fetch(
+      "https://my-memory-site.pages.dev/",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          user,
+          text,
+          image: img,
+          time: new Date().toLocaleString()
+        })
+      }
+    );
+
+    alert("投稿成功！");
+  }
 }
