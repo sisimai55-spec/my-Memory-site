11b69e8bedb9bee262bb05ec64a6d236bc0c2e32
sisimai55-spec <sisimai55@yahoo.co.jp>
2026-03-02 17:30:22 +0900

    Update view.js

--- js/view.js
+++ js/view.js
@@ -1,17 +1,21 @@
-async function load(){
+fetch("posts.json")
+.then(r=>r.json())
+.then(data=>{
 
- const res=await fetch(API_URL+"/api/posts");
- const posts=await res.json();
+  const area=document.getElementById("posts");
 
- const area=document.getElementById("posts");
+  data.reverse().forEach(p=>{
 
- posts.reverse().forEach(p=>{
-   area.innerHTML+=`
-   <div class="container">
-   <b>${p.user}</b><br>
-   ${p.text}
-   </div>`;
- });
-}
+    const div=document.createElement("div");
 
-load();
+    div.innerHTML =
+      "<b>"+p.user+"</b><br>"
+      +p.text+"<br>"
+      +(p.image ?
+        `<img src="${p.image}" width="200">`
+        :"");
+
+    area.appendChild(div);
+  });
+
+});
